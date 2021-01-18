5e8530a8bbd9cd8ab27afccbf17389e548de6209
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-01-18 10:59:55 -0600

    Updated gitignore

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -5,8 +5,10 @@
 #    --output_path data/unique-v1.jsonl \
 #    --min_jaccard 0.25
 
-python preprocess/add_references.py \
-    --input_path data/unique-v1.jsonl \
-    --output_path data/unique-ref-v1.jsonl
+#python preprocess/add_references.py \
+#    --input_path data/unique-v1.jsonl \
+#    --output_path data/unique-ref-v1.jsonl
 
-#    --article_cache data/unique-ref-articles-v1.json
+python preprocess/download_articles.py \
+    --input_path data/unique-ref-v1.jsonl \
+    --data/unique-ref-articles-v1.json
