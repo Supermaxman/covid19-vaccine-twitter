f53e14f753a414a80405303c615244e014e445cb
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-01-23 13:47:09 -0600

    BERTScore script added

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -12,19 +12,25 @@
 #python preprocess/download_articles.py \
 #    --input_path data/unique-ref-v1.jsonl \
 #    --output_path data/unique-ref-articles-v1.jsonl
-#
+
 #python preprocess/parse_articles.py \
 #    --input_path data/unique-ref-articles-v1.jsonl \
 #    --output_path data/unique-ref-parsed-articles-v1.jsonl
-#
+
 #python preprocess/add_articles.py \
 #    --input_path data/unique-ref-v1.jsonl \
 #    --articles_path data/unique-ref-parsed-articles-v1.jsonl \
 #    --output_path data/unique-art-v1.jsonl
 
-python preprocess/run_bert_score.py \
+#python preprocess/run_bert_score.py \
+#    --input_path data/unique-art-v1.jsonl \
+#    --misinfo_path data/misinfo.json \
+#    --output_path data/scores.json \
+#    --device cuda:4
+
+python preprocess/select_candidates.py \
     --input_path data/unique-art-v1.jsonl \
     --misinfo_path data/misinfo.json \
-    --output_path data/scores.json \
-    --device cuda:4
-
+    --score_path data/scores.json \
+    --output_path data/unique-art-v1-candidates.json \
+    --top_k 100
