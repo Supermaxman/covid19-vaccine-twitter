b289cd50a754904e50642b8c4783037b5ae29754
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-01-26 05:41:30 -0600

    BERTScore script added

--- preprocess/run_bert_score.py
+++ preprocess/run_bert_score.py
@@ -6,6 +6,12 @@ import transformers
 from bert_score import BERTScorer
 import numpy as np
 import random
+from tqdm import tqdm
+
+
+def divide_chunks(l, n):
+	for i in range(0, len(l), n):
+		yield l[i:i + n]
 
 
 def read_jsonl(path):
@@ -35,6 +41,7 @@ if __name__ == '__main__':
 	parser.add_argument('-bs', '--batch_size', default=32, type=int)
 	parser.add_argument('-mlp', '--max_length_percentile', default=95, type=int)
 	parser.add_argument('-s', '--seed', default=0, type=int)
+	parser.add_argument('-tc', '--total_chunks', default=5, type=int)
 	args = parser.parse_args()
 
 	np.random.seed(args.seed)
@@ -44,11 +51,16 @@ if __name__ == '__main__':
 	transformers.configuration_utils.logger.setLevel(logging.ERROR)
 	transformers.modeling_utils.logger.setLevel(logging.ERROR)
 
+	print('Loading tweets...')
 	tweets = read_jsonl(args.input_path)
+
+	chunk_size = int(np.ceil((len(tweets) / args.total_chunks)))
+
 	print(f'Total tweets read: {len(tweets)}')
 	with open(args.misinfo_path) as f:
 		misinfo = json.load(f)
 
+	print(f'Loading model: {args.model_type} ({args.num_layers}) on {args.device}...')
 	# https://arxiv.org/abs/1904.09675
 	# https://github.com/Tiiiger/bert_score
 	scorer = BERTScorer(
@@ -57,31 +69,35 @@ if __name__ == '__main__':
 		device=args.device
 	)
 	max_chars = int(np.percentile([len(t['full_text']) for t in tweets], args.max_length_percentile))
-	tweet_texts = []
-	m_texts = []
-	for t in tweets:
-		tweet_id = t['id']
-		tweet_text = t['full_text'][:max_chars]
-		for m_id, m in misinfo.items():
-			m_text = m['text']
-			tweet_texts.append(tweet_text)
-			m_texts.append(m_text)
 
-	t_p, t_r, t_f1 = scorer.score(
-		cands=tweet_texts,
-		refs=m_texts,
-		verbose=True,
-		batch_size=args.batch_size
-	)
-	t_f1 = t_f1.view(len(tweets), len(misinfo)).detach().numpy()
+	print(f'{args.max_length_percentile}-percentile tweet character length: {max_chars}')
 	scores = {}
-	for t, tweet_scores in zip(tweets, t_f1):
-		tweet_id = t['id']
-		t_scores = {}
-		for (m_id, m), m_score in zip(misinfo.items(), tweet_scores):
-			m_score = float(m_score)
-			t_scores[m_id] = m_score
-		scores[tweet_id] = t_scores
+	for chunk_idx, chunk_tweets in enumerate(divide_chunks(tweets, chunk_size)):
+		print(f'Processing chunk {chunk_idx+1}/{args.total_chunks} ({len(chunk_tweets)})...')
+		tweet_texts = []
+		m_texts = []
+		for t in chunk_tweets:
+			tweet_id = t['id']
+			tweet_text = t['full_text'][:max_chars]
+			for m_id, m in misinfo.items():
+				m_text = m['text']
+				tweet_texts.append(tweet_text)
+				m_texts.append(m_text)
+
+		t_p, t_r, t_f1 = scorer.score(
+			cands=tweet_texts,
+			refs=m_texts,
+			verbose=True,
+			batch_size=args.batch_size
+		)
+		t_f1 = t_f1.view(len(chunk_tweets), len(misinfo)).detach().numpy()
+		for t, tweet_scores in zip(chunk_tweets, t_f1):
+			tweet_id = t['id']
+			t_scores = {}
+			for (m_id, m), m_score in zip(misinfo.items(), tweet_scores):
+				m_score = float(m_score)
+				t_scores[m_id] = m_score
+			scores[tweet_id] = t_scores
 
 	with open(args.output_path, 'w') as f:
 		json.dump(scores, f)
