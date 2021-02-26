1898aa382f999f2bff0f9b9a4b7c61d740bf0123
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-02-25 22:31:24 -0600

    temperature parameter changes

--- experiments/queue.sh
+++ experiments/queue.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-#bash experiments/1.sh
-bash experiments/2.sh
-bash experiments/3.sh
-bash experiments/4.sh
+
+bash experiments/9.sh
+bash experiments/10.sh
+bash experiments/11.sh
+bash experiments/12.sh
+bash experiments/13.sh
+bash experiments/14.sh
+bash experiments/15.sh
+bash experiments/16.sh
