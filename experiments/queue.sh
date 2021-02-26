981196e36586f58ab7df40c21c7cba6ce3a1acac
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-02-26 17:08:11 -0600

    Experiments

--- experiments/queue.sh
+++ experiments/queue.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-
-bash experiments/18.sh
-bash experiments/19.sh
-bash experiments/20.sh
+bash experiments/21.sh
+bash experiments/22.sh
+bash experiments/23.sh
+bash experiments/24.sh
+bash experiments/25.sh
+bash experiments/26.sh
