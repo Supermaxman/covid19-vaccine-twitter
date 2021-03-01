84612758ad5cb4f9ab4e3564fc6ba6b5695fc0be
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-02-28 19:43:34 -0600

    Binary loss added

--- experiments/bm25-baseline-zero.sh
+++ experiments/bm25-baseline-zero.sh
@@ -2,6 +2,11 @@
 
 DATASET=v2
 DATASET_PATH=data/${DATASET}
+
+MISINFO_THRESHOLD_MIN=0.00
+MISINFO_THRESHOLD_MAX=5.00
+MISINFO_THRESHOLD_STEP=0.0005
+
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.7.10-0.fc30.x86_64/
 
 mkdir ${DATASET_PATH}/test-misinfo-index-data
@@ -43,6 +48,9 @@ python identify/score_predict.py \
   --val_path ${DATASET_PATH}/dev.jsonl \
   --misinfo_path ${DATASET_PATH}/test_misinfo.json \
   --model_name bm25-scores-zero \
+  --threshold_min ${MISINFO_THRESHOLD_MIN} \
+  --threshold_max ${MISINFO_THRESHOLD_MAX} \
+  --threshold_step ${MISINFO_THRESHOLD_STEP} \
   --train_score_path ${DATASET_PATH}/train-bm25-scores.json \
   --val_score_path ${DATASET_PATH}/dev-bm25-scores.json
 
@@ -51,5 +59,8 @@ python identify/score_predict.py \
   --val_path ${DATASET_PATH}/test.jsonl \
   --misinfo_path ${DATASET_PATH}/test_misinfo.json \
   --model_name bm25-scores-zero \
+  --threshold_min ${MISINFO_THRESHOLD_MIN} \
+  --threshold_max ${MISINFO_THRESHOLD_MAX} \
+  --threshold_step ${MISINFO_THRESHOLD_STEP} \
   --train_score_path ${DATASET_PATH}/train-bm25-scores.json \
   --val_score_path ${DATASET_PATH}/test-bm25-scores.json
