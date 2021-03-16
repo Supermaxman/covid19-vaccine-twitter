79fa5d799358a96fbe5cdb2cb33df7b9008d6028
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-03-16 15:16:49 -0500

    Experiments

--- rel/emb_utils.py
+++ rel/emb_utils.py
@@ -1,4 +1,5 @@
 
+import math
 from torch import nn
 import torch
 
@@ -125,3 +126,67 @@ class TransEEmbedding(nn.Module):
 		loss = torch.clamp(self.gamma + margin, min=0.0)
 		accuracy = (pos_energy.lt(neg_energy)).float().mean()
 		return loss, accuracy
+
+
+class RotatEEmbedding(nn.Module):
+	def __init__(self, hidden_size, emb_size, gamma, loss_norm=2):
+		super().__init__()
+		self.gamma = gamma
+		self.emb_size = emb_size
+		self.loss_norm = loss_norm
+		self.td_emb_size = self.emb_size // 2
+		self.e_emb_layer = nn.Linear(
+			hidden_size,
+			self.td_emb_size
+		)
+		self.e_proj_layer = nn.Linear(
+			hidden_size,
+			self.td_emb_size
+		)
+		self.r_emb_layer = nn.Linear(
+			hidden_size,
+			self.td_emb_size
+		)
+		self.score_func = nn.LogSigmoid()
+
+	def forward(self, source_embeddings, emb_type):
+		if emb_type == 'entity':
+			# [bsize * num_seq, emb_size]
+			ex_embs = self.e_emb_layer(source_embeddings)
+			ex_ims = self.e_proj_layer(source_embeddings)
+			ex_embs = torch.cat([ex_embs, ex_ims], dim=-1)
+			return ex_embs
+		elif emb_type == 'rel':
+			# [bsize * num_seq, emb_size]
+			r_embs = self.r_emb_layer(source_embeddings)
+			return r_embs
+		else:
+			raise ValueError(f'Unknown emb type: {emb_type}')
+
+	def energy(self, head, rel, tail):
+		h_re, h_im = head[..., :self.td_emb_size], head[..., self.td_emb_size:]
+		t_re, t_im = tail[..., :self.td_emb_size], tail[..., self.td_emb_size:]
+		r_phase = torch.tanh(rel) * math.pi
+		r_re = torch.cos(r_phase)
+		r_im = torch.sin(r_phase)
+
+		re_score = (h_re * r_re - h_im * r_im) - t_re
+		im_score = (h_re * r_im + h_im * r_re) - t_im
+		h_r_t_diff = torch.cat([re_score, im_score], dim=-1)
+
+		# l2 norm squared = sum of squares
+		if self.loss_norm == 1:
+			h_r_t_energy = torch.norm(h_r_t_diff, p=1, dim=-1, keepdim=False)
+		elif self.loss_norm == 2:
+			h_r_t_energy = (h_r_t_diff * h_r_t_diff).sum(dim=-1)
+		else:
+			raise ValueError(f'Unknown loss norm: {self.loss_norm}')
+		return h_r_t_energy
+
+	def loss(self, pos_energy, neg_energy):
+		pos_loss = -self.score_func(self.gamma - pos_energy)
+		neg_loss = -self.score_func(neg_energy - self.gamma)
+		loss = pos_loss + neg_loss
+		accuracy = (pos_energy.lt(neg_energy)).float().mean()
+		return loss, accuracy
+
--- rel/model_utils.py
+++ rel/model_utils.py
@@ -70,6 +70,13 @@ class CovidTwitterMisinfoModel(pl.LightningModule):
 				self.gamma,
 				self.emb_loss_norm
 			)
+		elif emb_model == 'rotate':
+			self.emb_model = RotatEEmbedding(
+				self.config.hidden_size,
+				self.emb_size,
+				self.gamma,
+				self.emb_loss_norm
+			)
 		else:
 			raise ValueError(f'Unknown embedding model: {emb_model}')
 
