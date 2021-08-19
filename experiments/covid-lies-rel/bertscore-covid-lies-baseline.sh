1e75d6b6bc05f727cc5cdcaf960d3f74bdae774e
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-08-19 13:03:31 -0500

    JBI revision baselines

--- experiments/covid-lies-rel/bertscore-covid-lies-baseline.sh
+++ experiments/covid-lies-rel/bertscore-covid-lies-baseline.sh
@@ -1,41 +1,41 @@
 #!/usr/bin/env bash
 
-DATASET=v1
+DATASET=covid-lies
 DATASET_PATH=data/${DATASET}
+NUM_SPLITS=5
 
-python preprocess/run_bert_score.py \
-    --input_path ${DATASET_PATH}/train.jsonl \
+SPLIT_FILES=""
+
+for (( SPLIT=1; SPLIT<=${NUM_SPLITS}; SPLIT++ )) do
+  echo "Training split ${SPLIT} model..."
+  MODEL_NAME=covid-twitter-v2-bertscore-covid-lies-scores-s${SPLIT}
+
+  python preprocess/run_bert_score.py \
+      --input_path ${DATASET_PATH}/train_s"${SPLIT}".jsonl \
+      --misinfo_path ${DATASET_PATH}/misinfo.json \
+      --output_path ${DATASET_PATH}/train_s"${SPLIT}"-bert-scores.json \
+      --device cuda:0 \
+      --batch_size 32 \
+      --total_chunks 1
+
+  python preprocess/run_bert_score.py \
+      --input_path ${DATASET_PATH}/test_s"${SPLIT}".jsonl \
+      --misinfo_path ${DATASET_PATH}/misinfo.json \
+      --output_path ${DATASET_PATH}/test_s"${SPLIT}"-bert-scores.json \
+      --device cuda:0 \
+      --batch_size 32 \
+      --total_chunks 1
+
+  python identify/score_predict.py \
+    --train_path ${DATASET_PATH}/train_s"${SPLIT}".jsonl \
+    --val_path ${DATASET_PATH}/test_s"${SPLIT}".jsonl \
     --misinfo_path ${DATASET_PATH}/misinfo.json \
-    --output_path ${DATASET_PATH}/train-bert-scores.json \
-    --device cuda:0 \
-    --batch_size 32
+    --model_name ${MODEL_NAME} \
+    --train_score_path ${DATASET_PATH}/train_s"${SPLIT}"-bert-scores.json\
+    --val_score_path ${DATASET_PATH}/test_s"${SPLIT}"-bert-scores.json
 
-python preprocess/run_bert_score.py \
-    --input_path ${DATASET_PATH}/dev.jsonl \
-    --misinfo_path ${DATASET_PATH}/misinfo.json \
-    --output_path ${DATASET_PATH}/dev-bert-scores.json \
-    --device cuda:0 \
-    --batch_size 32
+    SPLIT_FILES="${SPLIT_FILES},models/${MODEL_NAME}/predictions.jsonl"
+done
 
-python preprocess/run_bert_score.py \
-    --input_path ${DATASET_PATH}/test.jsonl \
-    --misinfo_path ${DATASET_PATH}/misinfo.json \
-    --output_path ${DATASET_PATH}/test-bert-scores.json \
-    --device cuda:0 \
-    --batch_size 32
-
-python identify/score_predict.py \
-  --train_path ${DATASET_PATH}/train.jsonl \
-  --val_path ${DATASET_PATH}/dev.jsonl \
-  --misinfo_path ${DATASET_PATH}/misinfo.json \
-  --model_name covid-twitter-v2-bertscore \
-  --train_score_path ${DATASET_PATH}/train-bert-scores.json\
-  --val_score_path ${DATASET_PATH}/dev-bert-scores.json
-
-python identify/score_predict.py \
-  --train_path ${DATASET_PATH}/train.jsonl \
-  --val_path ${DATASET_PATH}/test.jsonl \
-  --misinfo_path ${DATASET_PATH}/misinfo.json \
-  --model_name covid-twitter-v2-bertscore \
-  --train_score_path ${DATASET_PATH}/train-bert-scores.json\
-  --val_score_path ${DATASET_PATH}/test-bert-scores.json
+python identify/multi_split_eval.py \
+  --input_path ${SPLIT_FILES}
