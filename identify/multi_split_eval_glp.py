ca9aac01dd57db1620bb7f22f0d21b77428e0798
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-08-19 19:54:47 -0500

    JBI revision baselines

---
+++ identify/multi_split_eval_glp.py
@@ -0,0 +1,41 @@
+
+import argparse
+import json
+import torch
+
+from metric_utils import compute_f1
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--input_path', required=True)
+	args = parser.parse_args()
+	all_preds = []
+	for input_path in args.input_path.split(','):
+		if input_path:
+			print(f'{input_path}')
+			with open(input_path) as f:
+				# {'tweet_id': '1233992667366920192',
+				#   'm_id': '127',
+				#   'm_label': 0,
+				#   'm_pred': 0},
+				preds = json.load(f)[-1]['preds']
+				all_preds.extend(preds)
+
+	labels = []
+	model_preds = []
+	m_ids = []
+	t_ids = []
+	for p in all_preds:
+		labels.append(p['m_label'])
+		model_preds.append(p['m_pred'])
+		m_ids.append(p['m_id'])
+		t_ids.append(p['tweet_id'])
+
+	f1, p, r, _ = compute_f1(
+		torch.tensor(model_preds),
+		torch.tensor(labels),
+		threshold=0
+	)
+	print(f'P\tR\tF1\tT')
+	print(f'{p:.4f}\t{r:.4f}\t{f1:.4f}')
